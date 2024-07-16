4b3a223536bf471527a0734ad98b483222dbf7ed
Jayshree Moundekar <Jayashree.Moundekar@gmail.com>
2024-07-16 17:47:12 +0530

    American guy commit

---
+++ CucumberFrameworkJunitFramework/src/test/java/seleniumTest/SeleniumTest.java
@@ -0,0 +1,14 @@
+package seleniumTest;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	
+	public void landingPageProductName() {
+		System.out.println("this is landing page product name");
+}
+
+
+}
